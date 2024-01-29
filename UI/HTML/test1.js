f199b551cdfdf977aef2ce0497a7bad828f120e1
vishwas singh <88227880+Vishwas1919@users.noreply.github.com>
2024-01-29 20:54:14 +0530

    commit

--- UI/HTML/test1.js
+++ UI/HTML/test1.js
@@ -1,4 +1,3 @@
-console.log("Hii");
 const data=[
     {
         redtitle:'Popular',
@@ -8,7 +7,8 @@ const data=[
         fee:'$15.90',
         feetype:'*Free',
         institute:'/ASSETS/cloud.png',
-        rating:'<i class="fa-sharp fa-solid fa-star"></i>(43,435)'
+        rating:'<i class="fa-sharp fa-solid fa-star"></i> <i class="fa-sharp fa-solid fa-star"></i> <i class="fa-sharp fa-solid fa-star"></i> <i class="fa-sharp fa-solid fa-star"></i> (43,435)',
+        button:'Enroll Now'
 
     },
     {
@@ -17,58 +17,61 @@ const data=[
         type:'Communication ',
         duration:'Duration : 50Mins',
         fee:'$15.90',
-        feetype:'*Free',
-        institute:'/ASSETS/khan.png',
-        rating:'<i class="fa-sharp fa-solid fa-star"></i>(30,435)'
+        feetype:'',
+        institute:'/ASSETS/coursera.png',
+        rating:'<i class="fa-sharp fa-solid fa-star"></i> <i class="fa-sharp fa-solid fa-star"></i> <i class="fa-sharp fa-solid fa-star"></i> <i class="fa-sharp fa-solid fa-star"></i> (30,435)',
+        button:'Buy Now'
 
     },
     {
         redtitle:'Best Seller',
         image:"/ASSETS/image3.jpeg",
-        type:'Communication ',
+        type:'Communication',
         duration:'Duration : 1hour 12Mins',
         fee:'$10.90',
-        feetype:'*Free',
+        feetype:'',
         institute:'/ASSETS/khan.png',
-        rating:'<i class="fa-sharp fa-solid fa-star"></i>(28,435)'
+        rating:'<i class="fa-sharp fa-solid fa-star"></i> <i class="fa-sharp fa-solid fa-star"></i> <i class="fa-sharp fa-solid fa-star"></i> <i class="fa-sharp fa-solid fa-star"></i>(28,435)',
+        button:'Buy Now'
+
+    },
+    {
+        redtitle:'Best Seller',
+        image:"/ASSETS/image3.jpeg",
+        type:'Communication',
+        duration:'Duration : 1hour 12Mins',
+        fee:'$10.90',
+        feetype:'',
+        institute:'/ASSETS/khan.png',
+        rating:'<i class="fa-sharp fa-solid fa-star"></i> <i class="fa-sharp fa-solid fa-star"></i> <i class="fa-sharp fa-solid fa-star"></i> <i class="fa-sharp fa-solid fa-star"></i>(28,435)',
+        button:'Buy Now'
 
     }
 ]
+
 data.forEach((element,i) => {
+
     const main=document.querySelector(".main");
-        console.log(main);
-        const card=document.createElement("span");
-        card.classList='card';
-        const Educard=`
-        
-        <img src="${data[i].image}" alt="" width="150px" height="140px">
-        <span class="info">
-            <h4 class="title">${data[i].redtitletitle}</h4>
-            <span>${data[i].type}</span>
-            
-        
-        `
 
-        card.innerHTML+=Educard;
-        main.appendChild(card);
+    const card=document.createElement("div");
+    card.classList='card';
+    const Educard=`
+    <span class="info">
+        <p class="para">${data[i].redtitle}</p>
+    <img src="${data[i].image}">
+    <h3>${data[i].type}</h3>
+    <h4>${data[i].duration}</h4>
+    <span><strike>${data[i].fee}</strike></span>
+    <span>${data[i].feetype}</span>
+    <div  class="institute">
+        <img  src="${data[i].institute}">
+    </div>
+    <span class="rating">${data[i].rating}</span> 
+    </span>
+    <button class="dynamic-button" onclick="doNow()">${data[i].button}</button>
 
+    `
+    card.innerHTML+= Educard;
+    main.appendChild(card);
+    
 });
-// data.forEach((element,i) => {
-
-//     const main=document.querySelector(".main");
-
-//     const card=document.createElement("span");
-//     card.classList='card';
-//     const Educard=`
-//     <p>${data[i].redtitle}</p>
-//     <h4>${data[i].type}</h4>
-
-    
-    
-    
-//     `
-//     card.innerHTML+=Educard;
-//    // main.appendChild(card);
-//    main.appendChild(card)
-    
-
